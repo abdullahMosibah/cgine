1b59df4448e811ecbd4dc6fed66414ea9240c33c
abdullahMosaibah <abdullah.rm.2020@gmail.com>
2021-03-24 16:21:57 +0300

    0.0.2

--- config/settings/production.py
+++ config/settings/production.py
@@ -6,7 +6,9 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["cgine.io"])
+
+ALLOWED_HOSTS = ["immense-shelf-03290.herokuapp.com"]
+# ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["cgine.io"])
 
 # DATABASES
 # ------------------------------------------------------------------------------
