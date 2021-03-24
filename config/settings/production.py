e79b0415af17441ee85104bb43672ec975b66c4d
abdullahMosaibah <abdullah.rm.2020@gmail.com>
2021-03-24 22:35:25 +0300

    aws

--- config/settings/production.py
+++ config/settings/production.py
@@ -84,7 +84,8 @@ AWS_S3_CUSTOM_DOMAIN = env("DJANGO_AWS_S3_CUSTOM_DOMAIN", default=None)
 aws_s3_domain = AWS_S3_CUSTOM_DOMAIN or f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 # STATIC
 # ------------------------
-STATICFILES_STORAGE = "storages.backends.s3boto3.S3StaticStorage"
+
+STATICFILES_STORAGE = "cgine.utils.storages.StaticRootS3Boto3Storage"
 COLLECTFAST_STRATEGY = "collectfast.strategies.boto3.Boto3Strategy"
 STATIC_URL = f"https://{aws_s3_domain}/static/"
 # MEDIA
