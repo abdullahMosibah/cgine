d9cfc0f4091db8f2d303f80805535c0c5f554bf5
abdullahMosaibah <abdullah.rm.2020@gmail.com>
2021-05-16 20:24:47 +0300

    js probelm semi-hot fix

--- cgine/static/js/project.js
+++ cgine/static/js/project.js
@@ -1,15 +1,9 @@
-$(document).ready(function () {
-    $("#disclaimer").modal('show');
-});
+
 let playButtons = document.querySelectorAll(".play_button");
 const AudioContext = window.AudioContext || window.webkitAudioContext;
 const audioContext = new AudioContext();
-
 let test_audios = document.querySelectorAll(".test_audio");
 
-
-
-//event listener on the play button.
 
 for (let i = 0; i <= playButtons.length; i++) {
     let track = audioContext.createMediaElementSource(test_audios[i]);
@@ -35,76 +29,17 @@ for (let i = 0; i <= playButtons.length; i++) {
         playButtons[i].textContent = "play";
     }, false);
 }
+/* progress bar effect reference
+  let progressBar = document.getElementById("progress-bar");
 
 
+    // to add  the percentage progressBar effect
+    window.addEventListener('scroll', function () {
+      let max = document.body.scrollHeight - innerHeight;
+      progressBar.style.width = `${(pageYOffset / max) * 100}%`;
+    });
 
-
-let text_in_knowledge_blocks = document.querySelectorAll("#text-block");
-
-let disclaimer = document.getElementById("disclaimer");
-//--------------------- info box pop-up-------------------------------
-// info keyword that is located inside the info box
-let infoKeyword = document.getElementById("info_keyword");
-// the actual info box
-let infoBox = document.getElementById("info_box");
-// progress bar effect reference
-let progressBar = document.getElementById('progress-bar');
-// the buttons in the info box.
-let googleBtn = document.getElementById("google_btn");
-let googleScholarBtn = document.getElementById("google_scholar_btn");
-let popup_button = document.getElementById("popup_button");
-
-popup_button.addEventListener('click', function (){
-    $('#popup').modal('show');
-})
-
-//----------------------------------------------------------------------
-function google_search(evt) {
-    window.open(`https://www.google.com/search?q=${evt.target.myParam}`, '_blank');
-}
-
-function google_scholar_search(evt) {
-    window.open(`https://scholar.google.com/scholar?hl=en&as_sdt=0,5&q=${evt.target.myParam}`, "_blank");
-
-}
-
-
-
-function populate_info_box(keyword) {
-    infoKeyword.textContent = keyword;
-    //listener for the google normal search
-    googleBtn.addEventListener("click", google_search);
-    googleBtn.myParam = keyword;
-    //listener for the google scholar search.
-    googleScholarBtn.addEventListener('click', google_scholar_search);
-    googleScholarBtn.myParam = keyword;
-    // to make the info box disappear when clicked at any location at the document.
-    window.addEventListener('click', function () {
-        infoBox.classList.remove("show");
-    })
-    //googleBtn.removeEventListener("click", google_search(keyword));
-
-
-// for each span that contains text
-
-}
-
-text_in_knowledge_blocks.forEach(function (e) {
-    // double click event listener
-    e.addEventListener("dblclick", function (f) {
-        // to populate the info box + to make it appear when double clicked at any text.
-        populate_info_box(e.textContent);
-        infoBox.classList.add("show");
-
-    })
-});
-
-// to add  the percentage progressBar effect
-window.addEventListener('scroll', function () {
-    let max = document.body.scrollHeight - innerHeight;
-    progressBar.style.width = `${(pageYOffset / max) * 100}%`;
-});
-
+ */
 
 
 
